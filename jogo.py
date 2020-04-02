b973defcf40f319d9c87165a0e437c938b9e9b58
Renata Bottiglia <54960234+rbottiglia@users.noreply.github.com>
2020-04-02 03:37:13 -0300

    Update jogo.py
    primeiras aulas alura

--- jogo.py
+++ jogo.py
@@ -6,10 +6,13 @@ rodada = 1
 
 while (rodada <= total_de_tentativas):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute_str = input("Digite o seu número:")
+    chute_str = input("Digite um número entre 1 e 100:")
     print("Você digitou ", chute_str)
     chute = int(chute_str)
-    
+     
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100")
+        continue
     
     acertou = numero_secreto == chute
     maior = chute > numero_secreto
@@ -18,7 +21,7 @@ while (rodada <= total_de_tentativas):
     
     if(acertou):
       print("Você acertou!")
-    break
+      break
     else:
       if(maior):
         print("Você errou! O seu chute foi maior do que o número secreto.")
