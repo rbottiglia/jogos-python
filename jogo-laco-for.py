fc52cd4dc0e8d01b562e88fca2768eb4e7e2f9e1
Renata Bottiglia <54960234+rbottiglia@users.noreply.github.com>
2020-04-02 13:40:20 -0300

    Update jogo-laco-for.py

--- jogo-laco-for.py
+++ jogo-laco-for.py
@@ -27,5 +27,4 @@ for (rodada in range (1,total_de_tentativas + 1)):
       elif(menor):
         print("Você errou! O seu chute foi menor do que o número secreto.")
         
-    
 print("Fim do jogo!")
