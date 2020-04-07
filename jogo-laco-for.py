f37c57bc5c2f99954aa73b98fd537ee774dc77d7
Renata Bottiglia <54960234+rbottiglia@users.noreply.github.com>
2020-04-07 02:17:34 -0300

    Update jogo-laco-for.py

--- jogo-laco-for.py
+++ jogo-laco-for.py
@@ -1,6 +1,6 @@
 print("Bem vindo ao Jogo da Adivinhação!")
 
-numero_secreto = 42
+numero_secreto = round(random.random() * 100)
 total_de_tentativas = 3
 
 for (rodada in range (1,total_de_tentativas + 1)):
