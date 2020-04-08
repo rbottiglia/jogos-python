a5f7ecd98f98a8b54c62c5a9aac570b287d75fb4
Renata Bottiglia <54960234+rbottiglia@users.noreply.github.com>
2020-04-08 02:31:39 -0300

    Add files via upload

---
+++ forca.py
@@ -0,0 +1,9 @@
+def jogar():
+    print("****************************")
+    print("Esse é o jogo da forca :)!")
+    print("****************************")
+
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
