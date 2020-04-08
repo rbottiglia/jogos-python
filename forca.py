3426a84eba0c73539695175f9a954fbef37bafbf
Renata Bottiglia <54960234+rbottiglia@users.noreply.github.com>
2020-04-08 17:46:08 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -3,6 +3,23 @@ def jogar():
     print("Esse é o jogo da forca :)!")
     print("****************************")
 
+    palavra_secreta = "python"
+
+    enforcou = False
+    acertou = False
+
+    #enquanto (True) continua jogando
+    while(not enforcou and not acertou):
+        chute = input("Qual letra? ")
+        
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+              print("Encontre a letra {} na posição {}".format(letra, index))
+        index = index + 1
+
+        print("Jogando...")
+
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
