a696475fc9a14bb6735343c76fc579a11e6a4b07
Renata Bottiglia <54960234+rbottiglia@users.noreply.github.com>
2020-04-11 22:48:04 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -1,9 +1,24 @@
+import random
+
 def jogar():
     print("****************************")
     print("Esse é o jogo da forca :)!")
     print("****************************")
 
-    palavra_secreta = "banana".upper()
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0,len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
+    print(palavras)
+
     letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
